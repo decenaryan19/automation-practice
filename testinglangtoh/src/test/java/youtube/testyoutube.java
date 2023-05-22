6a896bcc39635904e9b22f00f6eee8e1acd15b41
Ryan Decena <119496639+decenaryan19@users.noreply.github.com>
2023-05-22 09:55:02 +0800

    update langs

--- testinglangtoh/src/test/java/youtube/testyoutube.java
+++ testinglangtoh/src/test/java/youtube/testyoutube.java
@@ -21,6 +21,7 @@ public class testyoutube {
 //		launch website
 		driver.get("https://www.youtube.com/");
 
+//		find element using Xpath
 		driver.findElement(By.xpath("//input[@id='search']")).sendKeys("walang imposible sa ating diyos");
 		driver.findElement(By.xpath("//button[@id='search-icon-legacy']")).click();
 		driver.findElement(By.xpath("//body/ytd-app[1]/div[1]/ytd-page-manager[1]/ytd-search[1]/div[1]/ytd-two-column-search-results-renderer[1]/div[1]/ytd-section-list-renderer[1]/div[2]/ytd-item-section-renderer[1]/div[3]/ytd-video-renderer[1]/div[1]")).click();
